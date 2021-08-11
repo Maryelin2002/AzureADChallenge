d46d9476b760662292d0f97d235995f19198b557
Maryelin2002 <64933036+Maryelin2002@users.noreply.github.com>
2021-08-11 13:15:47 -0400

    Complete

--- AzureADChallenge/AzureADChallenge/Controllers/GraphController.cs
+++ AzureADChallenge/AzureADChallenge/Controllers/GraphController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AzureADChallenge.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,31 +9,16 @@ using System.Threading.Tasks;
 
 namespace AzureADChallenge.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class GraphController : ControllerBase
     {
-        public MSGraphService graphService = new MSGraphService();
-        private static HttpClient httpClient = new HttpClient();
-        public string token;
+        public GraphService graphService = new GraphService();
 
-        [HttpPost("token")]
-        public async Task<string> GetTokenAsync()
+        [HttpGet]
+        public async Task<ActionResult> GetUsers()
         {
-            string url = ("https://login.microsoftonline.com/b378ae0f-f575-4ca8-ace3-81ace31f3706/oauth2/v2.0/token HTTP/1.1");
-
-            var result = await httpClient.GetAsync(url);
-            token = await result.Content.ReadAsStringAsync();
-
-            return token;
-        }
-
-        [HttpGet("user")]
-        public ActionResult GetUser()
-        {
-            var result = graphService.GetAllUsers(token);
-
-
+            var result = await graphService.GetAllUsers();
 
             return Ok(result);
         }
---
+++ AzureADChallenge/AzureADChallenge/Services/GraphService.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureADChallenge.Services
+{
+    public class GraphService
+    {
+        public static string clientId = "b33c5303-8cb2-416a-b0ef-b19daff437dd";
+        public static string tenantId = "b378ae0f-f575-4ca8-ace3-81ace31f3706";
+        public static string clientSecret = "k7_7Dt0_wI2gV8OHSSc~yM2ZL7.hulRqOW";
+
+        public async Task<IGraphServiceUsersCollectionPage> GetAllUsers()
+        {
+            var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            var graphClient = new GraphServiceClient(clientSecretCredential);
+
+            var users = await graphClient.Users.Request().GetAsync();
+
+            return users;
+        }
+    }
+}
